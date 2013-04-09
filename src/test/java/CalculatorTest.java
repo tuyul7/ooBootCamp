6c97d789cf9f380d364cd0311c0ffc950aa04721
Abigail Bangser <abangser@thoughtworks.com>
2013-04-08 22:24:01 -0500

    added assert to first calculator test

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -3,11 +3,15 @@ package test.java;
 import main.java.Calculator;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertEquals;
+
 public class CalculatorTest {
 
     @Test
     public void shouldSumMultipleValues(){
         Calculator calculator = new Calculator();
-        calculator.sum(1, 2);
+        assertEquals(3, calculator.sum(1, 2));
     }
+
+
 }

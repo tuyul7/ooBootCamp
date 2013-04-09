94c098822e44f01dec45b4827d13b165bbd3b5bc
Abigail Bangser <abangser@thoughtworks.com>
2013-04-08 22:40:30 -0500

    made sum for calculator pass with parameters

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,6 +3,7 @@ package main.java;
 public class Calculator {
 
     public int sum(int value1, int value2){
-        return 3;
+        return value1 + value2;
     }
+
 }
--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -8,10 +8,14 @@ import static junit.framework.Assert.assertEquals;
 public class CalculatorTest {
 
     @Test
-    public void shouldSumMultipleValues(){
+    public void shouldSumOnePlusTwo(){
         Calculator calculator = new Calculator();
         assertEquals(3, calculator.sum(1, 2));
     }
 
-
+    @Test
+    public void shouldSumTwoPlusThree(){
+        Calculator calculator = new Calculator();
+        assertEquals(5, calculator.sum(2, 3));
+    }
 }

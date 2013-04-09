e4769b917b0513a2cccf6a4f6e0af40f32b527f5
Abigail Bangser <abangser@thoughtworks.com>
2013-04-08 21:47:36 -0500

    re-do first test to assert area - failing

--- src/test/java/RectangleTest.java
+++ src/test/java/RectangleTest.java
@@ -3,13 +3,24 @@ package test.java;
 import main.java.Rectangle;
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static junit.framework.Assert.assertEquals;
+
 
 public class RectangleTest {
 
     @Test
-    public void shouldHaveAnAreaGreaterThanZero(){
+    public void shouldCalculateAreaFromWidthAndLength(){
+        double length = 2;
+        double width = 2;
+
         Rectangle rectangle = new Rectangle();
-        assertTrue(rectangle.area() > 0);
+        assertEquals(length * width, rectangle.area());
+
+    }
+
+    @Test
+    public void shouldHaveAnAreaGreaterThanZero(){
+//        Rectangle rectangle = new Rectangle();
+//        assertTrue(rectangle.area() > 0);
     }
 }

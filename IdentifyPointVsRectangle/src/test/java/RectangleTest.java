e0ccfcd3349a4f90b9c7440be06e4ff8a1a6ae23
Abigail Bangser <abangser@thoughtworks.com>
2013-04-15 21:49:03 -0500

    rounded out contains method test cases

--- IdentifyPointVsRectangle/src/test/java/RectangleTest.java
+++ IdentifyPointVsRectangle/src/test/java/RectangleTest.java
@@ -4,21 +4,36 @@ import main.java.Point;
 import main.java.Rectangle;
 import org.junit.Test;
 
+import static junit.framework.Assert.assertFalse;
 import static junit.framework.Assert.assertTrue;
 
 public class RectangleTest {
 
+    private Rectangle rectangle = new Rectangle(5, 5, 6, 8);;
+
     @Test
     public void shouldContainTopLeftCorner(){
-        Rectangle rectangle = new Rectangle(5, 5, 6, 8);
-
         assertTrue(rectangle.contains(new Point(2, 9)));
     }
 
     @Test
     public void shouldContainBottomRightCorner(){
-        Rectangle rectangle = new Rectangle(5, 5, 6, 8);
-
         assertTrue(rectangle.contains(new Point(8, 1)));
+    }
+
+    @Test
+    public void shouldNotContainPointAtZeroZero(){
+        assertFalse(rectangle.contains(new Point(0, 0)));
+    }
+
+    @Test
+    public void shouldContainCenterPoint(){
+        assertTrue(rectangle.contains(new Point(5, 5)));
+    }
+
+    @Test
+    public void shouldContainPointEvenIfInNegativeCoordinate(){
+        Rectangle rectangle = new Rectangle(1, 3, 4, 4);
+        assertTrue(rectangle.contains(new Point(-1, 5)));
     }
 }

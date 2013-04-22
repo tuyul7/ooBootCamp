c06a3a11ce6dcbb6bf4a5b1a7c255b9613a8c07f
Abigail Bangser <abangser@thoughtworks.com>
2013-04-21 21:16:08 -0500

    applied new convert method to main calls

--- Conversions/src/main/java/Converter.java
+++ Conversions/src/main/java/Converter.java
@@ -10,18 +10,6 @@ public class Converter {
         return bigDecimal.setScale(1, RoundingMode.CEILING).doubleValue();
     }
 
-    public double inchesToFeet(double inches) {
-        return round(inches / 12);
-    }
-
-    public double feetToInches(double val) {
-        return round(val * 12);
-    }
-
-    public Double cupsToTbsp(double val) {
-        return val * 8;
-    }
-
     public double convert(Pair pair, Object newType){
         double valInInches = pair.getKey() * pair.getValue();
         return round(valInInches / (Double) newType);
--- Conversions/src/main/java/main.java
+++ Conversions/src/main/java/main.java
@@ -5,11 +5,11 @@ public class main {
     public static void main(String[] args){
 
         double givenInches = 1;
-        double calculatedFeet = new Converter().inchesToFeet(givenInches);
+        double calculatedFeet = new Converter().convert(new Pair(Length.INCHES, givenInches), Length.FEET);
         System.out.println(givenInches + " inches equals " + calculatedFeet + " foot");
 
         double givenCups = 1;
-        double calculatedTbsp = new Converter().cupsToTbsp(givenCups);
+        double calculatedTbsp = new Converter().convert(new Pair(Volume.CUPS, givenCups), Volume.TABLESPOONS);
         System.out.println(givenCups + " cups equals " + calculatedTbsp + " tbsps");
     }
 }

71832db22862dd8367089d80f8c9fe4a46112bc5
Abigail Bangser <abangser@thoughtworks.com>
2013-06-23 15:06:19 -0400

    Learn to Code - Print Prime Numbers - second test

--- PrimeNumberPrinter/src/main/java/PrimeNumberGenerator.java
+++ PrimeNumberPrinter/src/main/java/PrimeNumberGenerator.java
@@ -2,6 +2,11 @@ package main.java;
 
 public class PrimeNumberGenerator {
     public Boolean isPrime(int max) {
+        for(int i = max - 1; i > 1; i--) {
+            if(max % i == 0) {
+                return false;
+            }
+        }
         return true;
     }
 }
--- PrimeNumberPrinter/src/test/java/PrimeNumberGeneratorTest.java
+++ PrimeNumberPrinter/src/test/java/PrimeNumberGeneratorTest.java
@@ -8,9 +8,15 @@ import static org.junit.Assert.assertThat;
 
 public class PrimeNumberGeneratorTest {
 
+    PrimeNumberGenerator primeNumberGenerator = new PrimeNumberGenerator();
+
     @Test
     public void shouldFindThreeAsAPrimeNumber(){
-        PrimeNumberGenerator primeNumberGenerator = new PrimeNumberGenerator();
         assertThat(primeNumberGenerator.isPrime(3), is(true));
+    }
+
+    @Test
+    public void shouldFindFourAsNotAPrimeNumber(){
+        assertThat(primeNumberGenerator.isPrime(4), is(false));
     }
 }

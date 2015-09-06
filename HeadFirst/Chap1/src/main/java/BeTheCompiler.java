cd40f11da1ff33be8e3377647fb5969b042532fb
tuyul7 <nelsonnababan130893@gmail.com>
2015-09-06 12:58:18 +0700

    Update BeTheCompiler.java

--- HeadFirst/Chap1/src/main/java/BeTheCompiler.java
+++ HeadFirst/Chap1/src/main/java/BeTheCompiler.java
@@ -8,7 +8,7 @@ public class BeTheCompiler {
                 int x = 1;
                 while ( x < 10 ) {
                     if ( x > 3) { System.out.println("big x"); }
-                    x = x + 1;
+                    //x = x + 1;
                 }
             }
         }
